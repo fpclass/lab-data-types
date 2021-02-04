a4bf6d902627a00bcd875b83864a840caa30533d
Michael Gale <m.gale@warwick.ac.uk>
2021-02-04 19:29:53 +0000

    Add solutions

--- src/Lab.hs
+++ src/Lab.hs
@@ -3,45 +3,57 @@
 -- Lab: Data types                                                            --
 --------------------------------------------------------------------------------
 
+{-# LANGUAGE RecordWildCards #-}
+
 module Lab where 
 
 --------------------------------------------------------------------------------
 
-data IntPos = IntPos Int Int 
+data IntPos = IntPos {
+    x :: Int,
+    y :: Int
+} -- deriving (Eq, Show)
 
 instance Eq IntPos where 
-    (IntPos x0 y0) == (IntPos x1 y1) = undefined 
+    (IntPos x0 y0) == (IntPos x1 y1) = x0 == x1 && y0 == y1 
 
+-- show (IntPos 4 7) => "IntPos 4 7"
 instance Show IntPos where 
-    show (IntPos x y) = undefined 
+    -- show (IntPos x y) = "IntPos " ++ show x ++ " " ++ show y
+    show IntPos{..} = concat [ "IntPos ", show x, " ", show y ]
 
 zeroPos :: IntPos 
-zeroPos = undefined 
+zeroPos = IntPos 0 0 
 
-x :: IntPos -> Int 
-x = undefined 
+-- x :: IntPos -> Int 
+-- x (IntPos v _) = v 
 
-y :: IntPos -> Int 
-y = undefined 
+-- y :: IntPos -> Int 
+-- y (IntPos _ v) = v 
 
 --------------------------------------------------------------------------------
 
-data Pos a = Pos a a 
+data Pos a = Pos {
+    left :: a,
+    top :: a
+}
+    -- deriving (Eq, Show)
 
 instance Eq a => Eq (Pos a) where 
-    (Pos x0 y0) == (Pos x1 y1) = undefined 
+    (Pos x0 y0) == (Pos x1 y1) = 
+        x0 == x1 && y0 == y1 
 
 instance Show a => Show (Pos a) where 
-    show (Pos x y) = undefined
+    show (Pos x y) = concat [ "Pos ", show x, " ", show y ]
 
 zero :: Num a => Pos a 
-zero = undefined 
+zero = Pos 0 0 
 
-left :: Pos a -> a 
-left = undefined 
+-- left :: Pos a -> a 
+-- left (Pos v _) = v 
 
-top :: Pos a -> a 
-top = undefined 
+-- top :: Pos a -> a 
+-- top (Pos _ v) = v 
 
 --------------------------------------------------------------------------------
 
@@ -55,6 +67,8 @@ doc =
     ]
 
 render :: [DocumentItem] -> Int -> String 
-render = undefined
+render [] _ = ""
+render (ListItem f : xs) n = f n ++ "\n" ++ render xs (n+1)
+-- render xs n = foldr (\(i,ListItem f) r -> f i ++ "\n" ++ r) "" (zip [n..] xs)
 
 --------------------------------------------------------------------------------
